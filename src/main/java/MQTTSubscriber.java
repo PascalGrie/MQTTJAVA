12486634fd93ebd7770a009dd169cac6ab14f45f
Pascal Grieser <Pa.grieser@web.de>
2023-03-15 13:13:29 +0100

    Build MQTT Subscriber.

---
+++ src/main/java/MQTTSubscriber.java
@@ -0,0 +1,54 @@
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class MQTTSubscriber {
+    public static void main(String[] args) {
+        String broker = "tcp://192.168.2.115:1883";
+        String topic = "esp32/Temperatur";
+        //String username = "emqx";
+        //String password = "public";
+        String clientid = "subscribe_client";
+        int qos = 0;
+
+        try {
+            MqttClient client = new MqttClient(broker, clientid, new MemoryPersistence());
+            // connect options
+            MqttConnectOptions options = new MqttConnectOptions();
+            //options.setUserName(username);
+            //options.setPassword(password.toCharArray());
+            options.setConnectionTimeout(60);
+            options.setKeepAliveInterval(60);
+            // setup callback
+            client.setCallback(new MqttCallback() {
+
+                public void connectionLost(Throwable cause) {
+                    System.out.println("connectionLost: " + cause.getMessage());
+                }
+
+                public void messageArrived(String topic, MqttMessage message) {
+                    System.out.println("topic: " + topic);
+                    System.out.println("Qos: " + message.getQos());
+                    System.out.println("message content: " + new String(message.getPayload()));
+
+                }
+
+                public void deliveryComplete(IMqttDeliveryToken token) {
+                    System.out.println("deliveryComplete---------" + token.isComplete());
+                }
+
+            });
+            client.connect(options);
+            client.subscribe(topic, qos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
